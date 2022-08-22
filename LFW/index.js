b8c6f04888262c8967c938aa9f81ed4c2d560255
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-08-22 17:38:38 +0900

    MVC_all 0822 정예현

--- LFW/index.js
+++ LFW/index.js
@@ -1,15 +1,15 @@
+const port = 8000;
+const path = require("path");
 const express = require("express");
 const app = express();
-const port = 8000;
 const bodyParser = require("body-parser");
-const path = require("path");
 const session = require("express-session");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true}));
-app.use( bodyParser.json() );
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,21 +20,21 @@ app.use(session({
 const userRouter = require("./routes/user");
 
 // 메인 페이지
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     const user = req.session.user;
 
-    if ( user != undefined ) {
-        res.render("index", {isLogin: true, user: user});
+    if (user != undefined) {
+        res.render("index", { isLogin: true, user: user });
     } else {
-        res.render("index", {isLogin: false});
+        res.render("index", { isLogin: false });
     }
 });
 
 // 로그아웃
-app.get("/logout", (req,res) => {
+app.get("/logout", (req, res) => {
     const user = req.session.user;
 
-    req.session.destroy(function(err){
+    req.session.destroy(function (err) {
         res.send(
             `<script>
                 alert('로그아웃 성공');
@@ -47,15 +47,15 @@ app.get("/logout", (req,res) => {
 app.get("/recipe", (req, res) => {
     const user = req.session.user;
 
-    if ( user != undefined ) {
-        res.render("recipe", {isLogin: true, user: user});
+    if (user != undefined) {
+        res.render("recipe", { isLogin: true, user: user });
     } else {
-        res.render("recipe", {isLogin: false});
+        res.render("recipe", { isLogin: false });
     }
 })
 
 app.use('/user', userRouter);
 
-app.listen(port, ()=>{
-    console.log( "Server Port : ", port );
+app.listen(port, () => {
+    console.log("Server Port : ", port);
 });

58adb2a8b17383468f45aae68c4562f15b420814
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-08-31 16:17:43 +0900

    0830 User_logic

--- LFW/controller/UserController.js
+++ LFW/controller/UserController.js
@@ -1,12 +1,11 @@
 const models = require("../model");
 
-
-/* 로그인 화면 */
+// 로그인 화면
 exports.login = (req, res) => {
     res.render("login");
 }
 
-// 로그인 
+// 로그인 실행
 exports.post_login = (req, res) => {
     models.User.findOne({
         where: { user_id: req.body.id, pw: req.body.pw }
@@ -15,8 +14,8 @@ exports.post_login = (req, res) => {
 
         if (result != null) {   // 로그인 성공
             req.session.regenerate(function () {
-                req.session.user = req.body.id;    //! 세션 셍성
-                req.session.save(function () {          //! 세션 저장
+                req.session.user = req.body.id;     //! 세션 셍성
+                req.session.save(function () {      //! 세션 저장
                     res.send({ return: true, msg: '로그인 성공' });
                 })
             })
@@ -26,35 +25,81 @@ exports.post_login = (req, res) => {
     })
 }
 
-// 로그아웃
+//로그아웃 실행
 exports.logout = (req, res) => {
-    req.session.destroy(function (err) {
-        if (err) throw err;
+    req.session.destroy(function () {
         res.send(`로그아웃 성공`);
     });
 }
-// /* 세션확인 - 로그아웃 */
-// app.get('/logout', (req, res) => {
-//     const user = req.session.user;
-
-//     req.session.destroy(function (err) {
-//         res.send(
-//             `<script>
-//                 alert('로그아웃 성공');
-//                 location.href='/';
-//             </script>`
-//         );
-//     });
-// });
 
 
-//^ 회원가입
-/* 회원가입 화면 */
+
+
+
+// 아이디 찾기 화면
+exports.find_id = (req, res) => {
+    res.render("find_id.ejs");
+}
+// 아이디 찾기 실행
+exports.post_find_id = (req, res) => {
+    models.User.findOne({
+        where: { name: req.body.name, email: req.body.email }
+    }).then((result) => {
+        // console.log('아이디찾기 실행 :', result);
+        if (result != null) {
+            res.send({ return: true, user_id: result.user_id });
+        } else {
+            res.send({ return: false, msg: '아이디를 찾을 수 없습니다.' });
+        }
+    })
+}
+
+// 비밀번호 찾기 화면
+exports.find_pw = (req, res) => {
+    res.render("find_pw");
+}
+// 비밀번호 찾기 실행
+exports.post_find_pw = (req, res) => {
+    models.User.findOne({
+        where: { user_id: req.body.user_id, email: req.body.email }
+    }).then((result) => {
+        if (result != null) {
+            req.session.user = req.body.id;    //! 세션 셍성
+            req.session.save(function () {          //! 세션 저장
+                // 로그인처리 & pw변경 페이지에 user_id 전달
+                res.send({ isLogin: true, return: true, msg: '로그인 성공' });
+            })
+        } else {
+            res.send({ return: false, msg: '비밀번호를 찾을 수 없습니다.' });
+        }
+    })
+}
+
+// 비밀번호 변경 화면
+exports.update_pw = (req, res) => {
+    res.render("update_pw");
+}
+
+// 비밀번호 변경 실행
+exports.updated_pw = (req, res) => {
+    let newPW = {
+        pw: req.body.pw
+    };
+
+    models.User.update(newPW, { where: { user_id: req.session.user } })
+        .then((result) => {
+            res.send({ pw: result.pw });
+        });
+}
+
+
+
+// 회원가입 화면
 exports.membership = (req, res) => {
     res.render("membership");
 }
 
-/* 회원가입 시, id 중복확인 */
+// 아이디 중복검사
 exports.id_check = (req, res) => {
     console.log("id_check");
     models.User.findOne({
@@ -70,7 +115,7 @@ exports.id_check = (req, res) => {
         })
 }
 
-/* 회원가입 시, 닉네임 중복확인 */
+// 닉네임 중복검사
 exports.nick_check = (req, res) => {
     console.log("nick_check");
     models.User.findOne({
@@ -86,7 +131,7 @@ exports.nick_check = (req, res) => {
         })
 }
 
-/* 회원가입 시, User 정보 저장 */
+//회원가입 실행
 exports.post_membership = (req, res) => {
     console.log(req.body);
 
@@ -112,90 +157,31 @@ exports.post_membership = (req, res) => {
 }
 
 
-// id 찾기
-exports.find_id = (req, res) => {
-    res.render("find_id.ejs");
-}
-exports.post_find_id = (req, res) => {
-    models.User.findOne({
-        where: { name: req.body.name, email: req.body.email }
-    }).then((result) => {
-        // console.log('아이디찾기 실행 :', result);
-        if (result != null) {
-            res.send({ return: true, user_id: result.user_id });
-        } else {
-            res.send({ return: false, msg: '아이디를 찾을 수 없습니다.' });
-        }
-    })
-}
 
-// pw 찾기
-exports.find_pw = (req, res) => {
-    res.render("find_pw");
-}
-exports.post_find_pw = (req, res) => {
-    models.User.findOne({
-        where: { user_id: req.body.user_id, email: req.body.email }
-    }).then((result) => {
-        if (result != null) {
-            req.session.user = req.body.id;    //! 세션 셍성
-            req.session.save(function () {          //! 세션 저장
-                // 로그인처리 & pw변경 페이지에 user_id 전달
-                res.send({ isLogin: true, return: true, msg: '로그인 성공' });
-            })
-        } else {
-            res.send({ return: false, msg: '비밀번호를 찾을 수 없습니다.' });
-        }
-    })
-}
 
-// pw 변경
-exports.pw_update = (req, res) => {
-    let newPW = {
-        pw: req.body.pw
-    };
-    models.User.update(newPW, { where: { user_id: req.session.user } })
-        .then((result) => {
-            res.send({ pw: result.pw });
-        });
-}
-
-// profile
+// profile 화면
 exports.profile = (req, res) => {
-    const user = req.session.user;
-    if (user != undefined) {
-        models.User.findOne({ where: { user_id: req.body.user_id } })
-            .then((result) => {
-                res.render("profile", { isLogin: true, user: user, user_id: result.user_id, name: result.name, tel: result.tel, email: result.email });
-            })
-    } else {
-        res.redirect("/user/login");
-    }
-}
-
-// update
-exports.update_page = (req, res) => {
-    const user = req.session.user;
-    models.User.findOne({ where: { user_id: req.body.user_id } })
+    models.User.findOne({ where: { user_id: req.session.user } })
         .then((result) => {
-            res.render("update_page", { isLogin: true, user: user, user_id: result.user_id, name: result.name, pw: result.pw, tel: result.tel, email: result.email });
-        });
+            res.render("profile", { isLogin: true, user: req.session.user, user_id: result.user_id, pw: result.pw, name: result.name, tel: result.tel, email: result.email });
+        })
 }
 
+// 회원정보수정
 exports.update = (req, res) => {
-    let obj = {
+    let user = {
         pw: req.body.pw,
         name: req.body.name,
         tel: req.body.tel,
         email: req.body.email
     }
-    models.User.update(obj, { where: { id: req.body.id } })
+    models.User.update(user, { where: { id: req.body.id } })
         .then((result) => {
             res.send("회원정보가 수정되었습니다.")
         })
 }
 
-// delete
+// 회원탈퇴
 exports.delete = (req, res) => {
     console.log(req.body);
     models.User.destroy({ where: { id: req.body.id } })
